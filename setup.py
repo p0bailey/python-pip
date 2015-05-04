f7289916f862deb9dbdef2f35c0b59ae2b03e796
p0bailey <phillip@bailey.st>
2015-05-04 18:42:52 +0100

    Github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(name='dummy',
       version='0.1',
       description='Dummy words',
-      url='git@bitbucket.org:p0bailey/python_pip.git',
+      url='git@github.com:p0bailey/python_pip.git',
       author='Phillip Bailey',
       author_email='phillip@bailey.st',
       license='MIT',
